9c223457f8202a5442632e3bbae50ee3181f2d53
Thiago Victor <tiagovictor674@gmail.com>
2024-11-27 21:10:54 -0300

    Melhora-Topo-css

--- topo.php
+++ topo.php
@@ -1,4 +1,5 @@
 <div id="topo">
     <h1>YOU COMPUITER</h1>
+    <link href="https://fonts.googleapis.com/css2?family=Tiny5&display=swap" rel="stylesheet">
 </div>
 <link rel="stylesheet" href="css/topo.css">
