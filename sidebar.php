47be287de0ff9e6f5206c4e508b9dabff44edcad
Thiago Victor <tiagovictor674@gmail.com>
2024-11-30 12:11:45 -0300

    Sidebar-de-categorias

---
+++ sidebar.php
@@ -0,0 +1,23 @@
+<?php
+include_once 'topo.php';
+include_once 'menu.php';
+?>
+
+<div class="sidebar">
+
+<h2>Categorias</h2>
+        <ul>
+            <li><a href="#">Categoria 1</a></li>
+            <li><a href="#">Categoria 2</a></li>
+            <li><a href="#">Categoria 3</a></li>
+            <li><a href="#">Categoria 4</a></li>
+            <li><a href="#">Categoria 5</a></li>
+            <li><a href="#">Categoria 6</a></li>
+        </ul>
+</div>
+
+<link rel="stylesheet" href="css/style.css">
+
+<?php
+include_once 'rodape.php';
+?>
