abb2d60a18088fac9f6f7cd31145b7e48a8daa5b
Thiago Victor <tiagovictor674@gmail.com>
2024-12-02 17:32:04 -0300

    Correçao-de-bug-sidebar

--- index.php
+++ index.php
@@ -3,6 +3,20 @@ include_once 'topo.php';
 include_once 'menu.php';
 ?>
 
+<body>
+
+<div class="sidebar">
+  <ul>
+    <?php
+    $categorias = ["Categoria 1", "Categoria 2", "Categoria 3"];
+    foreach ($categorias as $categoria) {
+        echo "<li><a href='#'>{$categoria}</a></li>";
+    }
+    ?>
+  </ul>
+</div>
+</body>
+
 <head>
     <link href="https://fonts.googleapis.com/css2?family=Press+Start+2P&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="css/style.css">
