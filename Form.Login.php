91677a3c884e3fde9740e3e13617937775ff5419
MIGUELLEMOS777 <miguellemosc2022@gmail.com>
2024-12-03 20:14:32 -0300

    css login e registro

--- Form.Login.php
+++ Form.Login.php
@@ -3,12 +3,20 @@ include_once 'topo.php';
 include_once 'menu.php';
 ?>
 
-<form action="Registro.Login/Login.php" method="post"><br>
-Email: <input type="text" name="email"><br>
-Senha: <input type="text" name="senha"><br>
-<input type="submit" value="entrar">
+<div class="Login">
+    <h2>Login</h2>
+    <form action="Registro.Login/Login.php" method="post">
+        <label for="email">Email:</label>
+        <input type="email" id="email" name="email" placeholder="Digite seu email" required><br>
 
-<li><a href="Form.registro.php">Cadastre-se</a></li>
+        <label for="senha">Senha:</label>
+        <input type="password" id="senha" name="senha" placeholder="Digite sua senha" required><br>
+
+        <input type="submit" value="Entrar">
+        <a href="Form.registro.php">Cadastre-se</a>
+    </form>
+</div>
+<link rel="stylesheet" href="css/style.css">
 <?php
 include_once 'rodape.php';
 ?>
--- Form.registro.php
+++ Form.registro.php
@@ -2,16 +2,38 @@
 include_once 'topo.php';
 include_once 'menu.php';
 ?>
-<form action="Registro.Login/Registro.php" method="post"><br>
-Nome Completo: <input type="text" name="nome" required><br>
-Email: <input type="text" name="email" required><br>
-Senha: <input type="text" name="senha" required><br>
-CEP: <input type="number" name="CEP" required><br>
-Rua: <input type="text" name="rua" required><br>
-Numero: <input type="number" name="numero" required><br>
-Complemento: <input type="text" name="complemento"><br>
-Telefone: <input type="number" name="telefone" required><br>
-<input type="submit" value="Cadastrar">
+
+<div class="registro">
+    <h2>Registro</h2>
+    <form action="Registro.Login/Registro.php" method="post">
+        <label for="nome">Nome Completo:</label>
+        <input type="text" id="nome" name="nome" placeholder="Digite seu nome completo" required><br>
+
+        <label for="email">Email:</label>
+        <input type="email" id="email" name="email" placeholder="Digite seu email" required><br>
+
+        <label for="senha">Senha:</label>
+        <input type="password" id="senha" name="senha" placeholder="Crie uma senha" required><br>
+
+        <label for="cep">CEP:</label>
+        <input type="text" id="cep" name="CEP" placeholder="Digite seu CEP" required><br>
+
+        <label for="rua">Rua:</label>
+        <input type="text" id="rua" name="rua" placeholder="Digite sua rua" required><br>
+
+        <label for="numero">Número:</label>
+        <input type="number" id="numero" name="numero" placeholder="Digite o número" required><br>
+
+        <label for="complemento">Complemento:</label>
+        <input type="text" id="complemento" name="complemento" placeholder="Opcional"><br>
+
+        <label for="telefone">Telefone:</label>
+        <input type="tel" id="telefone" name="telefone" placeholder="Digite seu telefone" required><br>
+
+        <input type="submit" value="Cadastrar">
+    </form>
+</div>
+<link rel="stylesheet" href="css/style.css">
 <?php
 include_once 'rodape.php';
 ?>
